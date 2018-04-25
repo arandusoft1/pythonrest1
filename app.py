b33889ce7192ad68eaf49963dc7a7249f205146a
arandusoft1 <38497767+arandusoft1@users.noreply.github.com>
2018-04-25 19:24:32 -0300

    prueba ver cod

--- app.py
+++ app.py
@@ -80,8 +80,7 @@ def create_task():
     
     conn = psycopg2.connect(database='d3fkm1msg7kiub',user='wdtetudvoejjev',password='b7fefda1a504e80018b763ba3d8bcb94804c54dfff9a3372b4a70ee042dadf22', host='ec2-54-83-1-94.compute-1.amazonaws.com')
     rec = conn.cursor()
-    #rec.execute("select codigo from Empresas where nombre = %s and sucursal = %s",(nom,suc))
-    rec.execute("select COUNT(codigo) from Empresas;")
+    rec.execute("select codigo from Empresas where nombre = %s and sucursal = %s",(nom,suc))
     rows = rec.fetchall()
     
     cod = 0
@@ -91,8 +90,8 @@ def create_task():
     end
     
     rec.close()
-    
-    cur = conn.cursor()
+    return cod, 201
+    """cur = conn.cursor()
     
     
     if cod > 0:
@@ -113,7 +112,7 @@ def create_task():
     cur.close()
     conn.close()
     
-    return jsonify({'task': task}), 201
+    return jsonify({'task': task}), 201"""
 
 if __name__ == '__main__':
     app.run(debug = True)
