20d3d19d84c4604e26537d93fb40399e6a0efb24
arandusoft1 <38497767+arandusoft1@users.noreply.github.com>
2018-04-25 10:36:44 -0300

    title con ""

--- app.py
+++ app.py
@@ -104,7 +104,7 @@ def get_task(task_id):
 
 @app.route('/todo/api/v1.0/tasks', methods=['POST'])
 def create_task():
-    if not request.json or not 'title' in request.json:
+    if not request.json or not "title" in request.json:
         abort(400)
     task = {
         'id': tasks[-1]['id'] + 1,
