0d2083ff2eb955282a961887cbb991bd67012423
arandusoft1 <38497767+arandusoft1@users.noreply.github.com>
2018-04-25 11:48:24 -0300

    saque comillas '' por ""

--- app.py
+++ app.py
@@ -104,12 +104,12 @@ def get_task(task_id):
 
 @app.route('/todo/api/v1.0/tasks', methods=['POST'])
 def create_task():
-    if not request.json or not 'title' in request.json:
+    if not request.json or not "title" in request.json:
         abort(400)
     task = {
-        'id': tasks[-1]['id'] + 1,
-        'title': request.json['title'],
-        'description': request.json.get('description', ""),
+        'id': tasks[-1]["id"] + 1,
+        'title': request.json["title"],
+        'description': request.json.get("description", ""),
         'done': False
     }
     tasks.append(task)
