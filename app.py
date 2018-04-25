6d89384cd71e76470c6c0fb57482a8239a087fae
arandusoft1 <38497767+arandusoft1@users.noreply.github.com>
2018-04-25 17:44:50 -0300

    solo devuelvo task no taskssss

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ def create_task():
         "CantPrecio": 80
     }
     tasks.append(task)
-    return jsonify({'tasks': tasks}), 201
+    return jsonify({'task': task}), 201
     #conn = psycopg2.connect(database='d3fkm1msg7kiub',user='wdtetudvoejjev',password='b7fefda1a504e80018b763ba3d8bcb94804c54dfff9a3372b4a70ee042dadf22', host='ec2-54-83-1-94.compute-1.amazonaws.com')
     
     
