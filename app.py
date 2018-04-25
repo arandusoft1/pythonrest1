85791e04fb04090776eb7349c799b1d9f2a31b0d
arandusoft1 <38497767+arandusoft1@users.noreply.github.com>
2018-04-25 10:22:02 -0300

    post get and get()

--- app.py
+++ app.py
@@ -66,16 +66,15 @@
 
 #Hasta aca lo mio
 
-#!flask/bin/python
-#import bottle
+
 from flask import Flask, jsonify
-#from bottle import route,run,Bottle,template
-#import json
-
-
-#bottle.debug(True)
+from flask import abort, make_response, request
 
 app = Flask(__name__)
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 tasks = [
     {
@@ -95,6 +94,26 @@ tasks = [
 @app.route('/todo/api/v1.0/tasks', methods=['GET'])
 def get_tasks():
     return jsonify({'tasks': tasks})
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
+
+@app.route('/todo/api/v1.0/tasks', methods=['POST'])
+def create_task():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify({'task': task}), 201
 
 if __name__ == '__main__':
 	run(host='0.0.0.0',port=argv[1])
