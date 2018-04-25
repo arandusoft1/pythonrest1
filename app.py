732febac243e04ddde2c81948d742ddedb60e82c
arandusoft1 <38497767+arandusoft1@users.noreply.github.com>
2018-04-25 19:15:11 -0300

    sql count

--- app.py
+++ app.py
@@ -80,13 +80,14 @@ def create_task():
     
     conn = psycopg2.connect(database='d3fkm1msg7kiub',user='wdtetudvoejjev',password='b7fefda1a504e80018b763ba3d8bcb94804c54dfff9a3372b4a70ee042dadf22', host='ec2-54-83-1-94.compute-1.amazonaws.com')
     rec = conn.cursor()
-    rec.execute("select codigo from Empresas where nombre = %s and sucursal = %s",(nom,suc))
+    #rec.execute("select codigo from Empresas where nombre = %s and sucursal = %s",(nom,suc))
+    rec.execute("select COUNT(codigo) from Empresas;")
     rows = rec.fetchall()
     
     cod = 0
     
     for row in rows:
-        cod = int(row[0])
+        cod = row
     end
     
     rec.close()
