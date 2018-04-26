67dd1d22dcb1442b3db5277dc2feb7cbc01bbff7
arandusoft1 <38497767+arandusoft1@users.noreply.github.com>
2018-04-26 09:53:18 -0300

    put con empresa y () para if task

--- app.py
+++ app.py
@@ -57,12 +57,12 @@ def get_task(task_nom):
 
 ######################################################################################################################################
 
-@app.route('/todo/api/v1.0/tasks/<task_nom>/<task_suc>', methods=['GET'])
+"""@app.route('/todo/api/v1.0/tasks/<task_nom>/<task_suc>', methods=['GET'])
 def get_task(task_nom, task_suc):
     task = [task for task in tasks if task['Empresa'] == task_nom and task['Sucursal'] == task_suc ]
     if len(task) == 0:
         abort(404)
-    return jsonify({'task': task[0]})
+    return jsonify({'task': task[0]})"""
 
 ######################################################################################################################################
 @app.route('/todo/api/v1.0/tasks', methods=['POST'])
@@ -112,7 +112,7 @@ def update_task(task_nom):
     nom = task_nom  #request.json["Empresa"]
     suc = request.json["Sucursal"]
     
-    task = [task for task in tasks if task['Empresa'] == nom and task['Sucursal'] == suc]
+    task = [task for task in tasks if ( task['Empresa'] == nom and task['Sucursal'] == suc )]
     
     if len(task) == 0:
         abort(404)
