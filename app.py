93da8c1f8372d6278d439b5497be4fe960687842
arandusoft1 <38497767+arandusoft1@users.noreply.github.com>
2018-05-02 19:52:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -126,44 +126,6 @@ def Ult_Vig():
 		cont = cont + 1
 		
 	return render_template('ultimavigencia.tpl', ultact=ultact,empresas=empresas)
-#####################################################################################################################################
-@app.route('/UltimaVigencia')
-def Ult_Vig():
-	conn = psycopg2.connect(database='d3fkm1msg7kiub',user='wdtetudvoejjev',password='b7fefda1a504e80018b763ba3d8bcb94804c54dfff9a3372b4a70ee042dadf22', host='ec2-54-83-1-94.compute-1.amazonaws.com')
-	con = conn.cursor()
-	con.execute("select * from Empresas;")
-	rows = con.fetchall()
-	empresas= []
-	fmt = '%d/%m/%y %H:%M:%S'
-	ultact = "01/01/01 00:00:00"
-	d2 = datetime.strptime(ultact,fmt)
-	
-	for row in rows:
-		empresas.append({"Empresa": row[1],"Sucursal": row[2],"fVigencia": row[3],"CantPrecio": row[4]})
-		#fvig.append({"fVigencia": row[3]})
-		d1 = datetime.strptime(row[3],fmt)		
-		diffhora= ((d1-d2).seconds)/3600.0
-		diffdias= (d1-d2).days
-		
-		if diffdias > 0:
-			d2 = d1
-			ultact = row[3]
-		elif diffdias == 0:
-			if diffhora > 0:
-				d2 = d1
-				ultact = row[3]
-				
-	da1 = datetime.strptime(ultact,fmt) 
-	cont = 0	
-	
-	for elemento in empresas:
-				
-		if elemento["fVigencia"] == ultact:
-			empresas[cont]["color"] = "V"
-		
-		cont = cont + 1
-		
-	return render_template('ultimavigencia.tpl', ultact=ultact,empresas=empresas)
 	
 #####################################################################################################################################
 @app.route('/vigenciamenora24hs')
