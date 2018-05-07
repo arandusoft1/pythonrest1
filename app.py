d910b6b55e36df51fa83e1f049c29f5bd7e0002c
arandusoft1 <38497767+arandusoft1@users.noreply.github.com>
2018-05-07 11:50:14 -0300

    Update app.py

--- app.py
+++ app.py
@@ -94,8 +94,8 @@ def index():
 
 @app.route('/buscarsucursal', methods=['POST'])
 def buscar():
-	if not 'sucursal' in request.form:
-        	abort(400)
+	"""if not 'sucursal' in request.form:
+        	abort(400)"""
 	
 	conn = psycopg2.connect(database='d3fkm1msg7kiub',user='wdtetudvoejjev',password='b7fefda1a504e80018b763ba3d8bcb94804c54dfff9a3372b4a70ee042dadf22', host='ec2-54-83-1-94.compute-1.amazonaws.com')
 	con = conn.cursor()
