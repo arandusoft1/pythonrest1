7878a824686101085c9608b3e521516bbb65267f
arandusoft1 <38497767+arandusoft1@users.noreply.github.com>
2018-05-07 10:44:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -93,8 +93,8 @@ def index():
 
 @app.route('/buscarsucursal', methods=['POST'])
 def buscar():
-	#if not 'sucursal' in request.form:
-        #	abort(400)
+	if not 'sucursal' in request.form:
+        	abort(400)
 	
 	conn = psycopg2.connect(database='d3fkm1msg7kiub',user='wdtetudvoejjev',password='b7fefda1a504e80018b763ba3d8bcb94804c54dfff9a3372b4a70ee042dadf22', host='ec2-54-83-1-94.compute-1.amazonaws.com')
 	con = conn.cursor()
@@ -344,6 +344,38 @@ def get_task(task_nom):
     if len(task) == 0:
         abort(404)
     return jsonify({'Empresa': task})
+
+
+######################################################################################################################################
+
+@app.route('/ultiact', methods=['GET'])
+def ultiact():
+	
+    conn = psycopg2.connect(database='d3fkm1msg7kiub',user='wdtetudvoejjev',password='b7fefda1a504e80018b763ba3d8bcb94804c54dfff9a3372b4a70ee042dadf22', host='ec2-54-83-1-94.compute-1.amazonaws.com')
+	con = conn.cursor()
+	con.execute("select * from Empresas;")
+	rows = con.fetchall()
+	empresas= []
+	fmt = '%d/%m/%y %H:%M:%S'
+	ultact = "01/01/01 00:00:00"
+	d2 = datetime.strptime(ultact,fmt)
+	
+	for row in rows:
+		d1 = datetime.strptime(row[3],fmt)		
+		diffhora= ((d1-d2).seconds)/3600.0
+		diffdias= (d1-d2).days
+		
+		if diffdias > 0:
+			d2 = d1
+			ultact = row[3]
+		elif diffdias == 0:
+			if diffhora > 0:
+				d2 = d1
+				ultact = row[3]
+
+
+
+    return (ultact)
 
 ######################################################################################################################################
 
