408a86703996bbe5ecc6d895cd8581165e7b99f2
arandusoft1 <38497767+arandusoft1@users.noreply.github.com>
2018-05-07 11:41:01 -0300

    Update app.py

--- app.py
+++ app.py
@@ -351,7 +351,7 @@ def get_task(task_nom):
 
 ######################################################################################################################################
 
-"""@app.route('/ultiact', methods=['GET'])
+@app.route('/ultiact', methods=['GET'])
 def ultiact():
 		
 	conn = psycopg2.connect(database='d3fkm1msg7kiub',user='wdtetudvoejjev',password='b7fefda1a504e80018b763ba3d8bcb94804c54dfff9a3372b4a70ee042dadf22', host='ec2-54-83-1-94.compute-1.amazonaws.com')
@@ -375,7 +375,7 @@ def ultiact():
 				d2 = d1
 				ultact = row[3]
         
-	return jsonify({'Ultima actualizacion': ultact})"""
+	return jsonify({'Ultima actualizacion': ultact})
 
 ######################################################################################################################################
 
